329a3130d8c5c22772558cf4640f39c0c2b2ef17
diejbkll <s22026@gsm.hs.kr>
2023-02-17 23:07:40 +0900

    instagram post

--- app/src/main/java/com/example/instagram/Fragment/PostFragment.kt
+++ app/src/main/java/com/example/instagram/Fragment/PostFragment.kt
@@ -1,19 +1,132 @@
 package com.example.instagram.Fragment
 
+import android.content.Context
+import android.content.Intent
+import android.database.Cursor
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
-import com.example.instagram.R
+import com.bumptech.glide.Glide
+import com.example.instagram.InstaMainActivity
+import com.example.instagram.RetrofitService
+import com.example.instagram.databinding.FragmentPostBinding
+import okhttp3.MediaType
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Multipart
+import java.io.File
 
-class PostFragment : Fragment(){
+class PostFragment : Fragment() {
+
+    lateinit var binding: FragmentPostBinding
+
+    var imageUri: Uri? = null
+    var contentInput: String = ""
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_post, container, false)
+        binding = FragmentPostBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://mellowcode.org/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val retrofitService = retrofit.create(RetrofitService::class.java)
+
+        val selectedImageView = binding.selectedImg
+        val glide = Glide.with(activity as InstaMainActivity)
+
+        val imagePickerLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                imageUri = it.data!!.data
+                glide.load(imageUri).into(selectedImageView)
+            }
+        imagePickerLauncher.launch(
+            Intent(Intent.ACTION_PICK).apply {
+                this.type = MediaStore.Images.Media.CONTENT_TYPE
+            }
+        )
+
+        binding.selectedContent.doAfterTextChanged {
+            contentInput = it.toString()
+        }
+
+        binding.upload.setOnClickListener {
+            // uri -> file
+            val file = getRealFile(imageUri!!)
+
+            val requestFile = RequestBody.create(
+                // file의 종류(이미지, 영상)
+                MediaType.parse(
+                    (activity as InstaMainActivity).contentResolver.getType(imageUri!!)
+                ),
+                file
+            )
+            // file의 key를 정해줌
+            val body = MultipartBody.Part.createFormData("image", file!!.name, requestFile)
+            val content = RequestBody.create(MultipartBody.FORM, contentInput)
+            val header = HashMap<String, String>()
+            val sp = (activity as InstaMainActivity).getSharedPreferences(
+                "user_info",
+                Context.MODE_PRIVATE
+            )
+            val token = sp.getString("token", "")
+            header.put("Authorization", "token" + token!!)
+            retrofitService.uploadPost(header, body, content).enqueue(object : Callback<Any> {
+                override fun onResponse(call: Call<Any>, response: Response<Any>) {
+
+                }
+
+                override fun onFailure(call: Call<Any>, t: Throwable) {
+                }
+            })
+        }
+    }
+
+    // uri -> file로 변경
+    private fun getRealFile(uri: Uri): File? {
+        var uri: Uri? = uri
+        val projection = arrayOf(MediaStore.Images.Media.DATA)
+        if (uri == null) {
+            uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        }
+        var cursor: Cursor? = (activity as InstaMainActivity).getContentResolver().query(
+            uri!!,
+            projection,
+            null,
+            null,
+            MediaStore.Images.Media.DATE_MODIFIED + " desc"
+        )
+        if (cursor == null || cursor.getColumnCount() < 1) {
+            return null
+        }
+        val column_index: Int = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+        cursor.moveToFirst()
+        val path: String = cursor.getString(column_index)
+        if (cursor != null) {
+            cursor.close()
+            cursor = null
+        }
+        return File(path)
     }
 }
